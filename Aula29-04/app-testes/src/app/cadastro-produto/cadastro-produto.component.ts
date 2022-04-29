b7419421e578ccce10df6a4037132d052182aeb3
Silva <jady.silva@capgemini.com>
2022-04-29 17:17:32 -0300

    UPDATE EXERCISES

---
+++ Aula29-04/app-testes/src/app/cadastro-produto/cadastro-produto.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduto } from 'src/interfaces/interfaces';
+
+@Component({
+  selector: 'app-cadastro-produto',
+  templateUrl: './cadastro-produto.component.html',
+  styleUrls: ['./cadastro-produto.component.css']
+})
+export class CadastroProdutoComponent implements OnInit {
+
+  frase: string = "ola muNDo daNAdo";
+
+  produto: IProduto = {
+     id:0,
+     descricao: "Feijão preto",
+     preco: 7.85,
+     estoque: 9.8
+  }
+
+  hoje = new Date();
+
+  descricao: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
