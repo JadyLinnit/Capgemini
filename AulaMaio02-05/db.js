0601c73047b873ac5ad05bac643e41de0289016d
Silva <jady.silva@capgemini.com>
2022-05-02 22:40:53 -0300

    UPDATE EXERCISES

---
+++ AulaMaio02-05/db.js
@@ -0,0 +1,25 @@
+let clientes = [
+    {
+       id:1,
+       nome: 'Ana Silva',
+       telefone: 3243-4343,
+       limiteCredito: 3000
+
+    },
+    {
+       id:2,
+       nome: 'Jady Silva',
+       telefone: 3243-4343,
+       limiteCredito: 4000
+
+    },
+
+    {
+       id:3,
+       nome: 'João Silva',
+       telefone: 3243-4343,
+       limiteCredito: 2000
+
+    }
+]
+module.exports = clientes
---
+++ AulaMaio02-05/dbprodutos.js
@@ -0,0 +1,23 @@
+let produtos = [{
+
+    "descricao": "Arroz",
+    "preco": 10.50,
+    "estoque": 10,
+    "id": 1
+},
+{
+
+    "descricao": "Carne",
+    "preco": 7.89,
+    "estoque": 10,
+    "id": 2
+},
+{
+    "descricao": "Macarrão",
+    "preco": 15.0,
+    "estoque": 5,
+    "id": 3
+}
+]
+
+module.exports =  produtos 
